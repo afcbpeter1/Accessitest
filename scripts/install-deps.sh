862940e42d1555d3025fa16eefd72639baa9aee9
Peter <kirby.peter@hotmail.co.uk>
2026-01-12 17:31:37 +0000

    Fix Render build: use custom install script to disable opencollective postinstall

---
+++ scripts/install-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Custom install script that disables opencollective postinstall scripts
+# This prevents the 'find' command errors during Render deployment
+
+export DISABLE_OPENCOLLECTIVE=1
+npm install
+exit $?
+
